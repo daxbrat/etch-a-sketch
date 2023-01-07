3fa291d9a2ac1eafd4894b2c8b42aca8aa855cdd
daxbrat <dax.dacosta@gmail.com>
2023-01-07 17:52:59 -0600

    create 16x16 grid

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let board = document.querySelector(".board");
+  board.style.gridTemplateColumns = "repeat(16, 1fr)";
+  board.style.gridTemplateRows = "repeat(16, 1fr)";
+});
